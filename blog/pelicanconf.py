bdf26ce9a8948340b947e76dcf0f303f564fcaf1
Michel Hua <mycaule@users.noreply.github.com>
2022-10-15 20:50:39 +0530

    Blog using Pelican (#3)

---
+++ blog/pelicanconf.py
@@ -0,0 +1,24 @@
+AUTHOR = 'THM'
+SITENAME = 'Team THM'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Rome'
+
+DEFAULT_LANG = 'en'
+
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+LINKS = (('Pelican', 'https://getpelican.com/'),)
+
+SOCIAL = (('Artefact', 'https://www.artefact.com'),
+          ('Redis Ventures', 'https://redisventures.com'),)
+
+DEFAULT_PAGINATION = False
+
+#RELATIVE_URLS = True
