595726e56db536c17005aab5999513ef6de047d4
Michel Hua <mycaule@users.noreply.github.com>
2022-10-15 18:08:28 +0200

    docs

--- backend/vecsim_app/config.py
+++ backend/vecsim_app/config.py
@@ -6,10 +6,15 @@ API_DOCS = "/api/docs"
 OPENAPI_DOCS = "/api/openapi.json"
 INDEX_NAME = "papers"
 INDEX_TYPE = os.environ.get("VECSIM_INDEX_TYPE", "HNSW")
-REDIS_HOST = os.environ.get("REDIS_HOST", "redis-vector-db")
-REDIS_PORT = os.environ.get("REDIS_PORT", 6379)
-REDIS_DB = os.environ.get("REDIS_DB", 0)
-REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD", "testing123")
+REDIS_HOST = "redis-18742.c226.eu-west-1-3.ec2.cloud.redislabs.com"
+# os.environ.get("REDIS_HOST", "redis-vector-db")
+REDIS_PORT = 18742
+# os.environ.get("REDIS_PORT", 6379)
+REDIS_DB = "Michel-free-db"
+# os.environ.get("REDIS_DB", 0)
+REDIS_PASSWORD = "PtGSdcULLsIImHKMh6jYIeIcpZ2w7Vrl"
+# os.environ.get("REDIS_PASSWORD", "testing123")
+
 REDIS_URL = f"redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}"
 os.environ["REDIS_DATA_URL"] = REDIS_URL
 os.environ["REDIS_OM_URL"] = REDIS_URL
