d5e70d9cb2226eeb0406b766721858c7f6568938
henriquebrito <henrique.brito@artefact.com>
2022-11-03 19:21:28 +0000

    Added script to execute training pipeline

---
+++ scripts/retrain_model.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+timestamp=$(date +%Y%m%d%H%M%S)
+output_folder="../backend/model_outputs_$timestamp"
+data_location="./data/arxiv-metadata-oai-snapshot.json" # user config
+
+mkdir -p "$output_folder/model_outputs"
+
+PYTHONPATH=../backend python3 ../backend/generate_soft_labels.py \
+--data_location="$data_location" \
+--output_location="$output_folder"
