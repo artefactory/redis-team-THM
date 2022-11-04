b63aa7820b9cfc36fcf797d2af787a5381098e71
Michel Hua <mycaule@users.noreply.github.com>
2022-11-04 08:27:57 +0100

    .

--- backend/thm/models.py
+++ backend/thm/models.py
@@ -11,9 +11,3 @@ class Paper(HashModel):
     categories: str = Field(index=True)
     predicted_categories: Optional[str]
     year: str = Field(index=True)
-    # vector: str
-
-
-# FIXME Doesn't work with pydantic
-# File "pydantic/json.py", line 45, in pydantic.json.lambda
-# UnicodeDecodeError: 'utf-8' codec can't decode byte 0xe2 in position 0: invalid continuation byte
