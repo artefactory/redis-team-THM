b8a9100b5f88219b445dbaf73c0b2ba7d56d7ccb
Michel Hua <mycaule@users.noreply.github.com>
2022-11-06 13:02:57 +0100

    refacto to netflix metaflow

---
+++ scripts/pipeline.py
@@ -0,0 +1,24 @@
+from metaflow import FlowSpec, step, IncludeFile, Parameter
+from generate_index import run
+
+class GenerateIndexFlow(FlowSpec):
+    """
+    A flow to generate index from arXiv corpus.
+    """
+
+    @step
+    def start(self):
+        """
+        The start step:
+        1) Loads the arXiv corpus into pandas dataframe.
+
+        """
+
+    @step
+    def end(self):
+        "Ends the flow."
+        pass
+
+
+if __name__ == '__main__':
+    GenerateIndexFlow()
